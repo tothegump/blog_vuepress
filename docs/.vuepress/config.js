43916eb3e9122201f82725822536e87809f5276e
tothegump <tothegump@gmail.com>
2019-03-17 22:37:02 +0800

    add chinwag directory

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,6 +10,7 @@ module.exports = {
     nav: [
       { text: '主页', link: '/' },
       { text: 'distributed-system', link: '/distributed-system/' },
+      { text: 'chinwag', link: '/chinwag/' },
       { text: 'reading', link: '/reading/' },
       { text: 'blog', link: 'https://tothegump.github.io' },
     ]
